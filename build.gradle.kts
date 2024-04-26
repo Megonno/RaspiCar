1ea42e53b6592df156d24f26804ecf98887f9dfc
Köhler <raphael.koehler@student.mbo.schule>
2024-04-26 09:39:37 +0200

    basic connection

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,13 @@ val mApiGroupId = "de.megonno"
 val mApiVersion = "1.0.0"
 val main = "$mApiGroupId.$mApiArtifactId.${mApiName}Kt"
 
+val ktorVersion = "2.3.10"
+
 plugins {
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.serialization") version "1.9.22"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     application
-    `maven-publish`
 }
 
 group = mApiGroupId
@@ -18,17 +19,20 @@ version = mApiVersion
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
-    maven("https://repo.getfdp.today/")
 }
 
 dependencies {
-    implementation("com.pi4j:pi4j-ktx:2.4.0")
-    implementation("com.pi4j:pi4j-core:2.3.0")
-    implementation("com.pi4j:pi4j-plugin-raspberrypi:2.3.0")
-    implementation("com.pi4j:pi4j-plugin-pigpio:2.3.0")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
+
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
+    implementation("io.ktor:ktor-client-cio:$ktorVersion")
+    implementation("io.ktor:ktor-client-websockets:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-xml:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-cbor:$ktorVersion")
+    implementation("io.ktor:ktor-serialization-kotlinx-protobuf:$ktorVersion")
+    implementation("io.ktor:ktor-client-logging:$ktorVersion")
 }
 
 kotlin {
