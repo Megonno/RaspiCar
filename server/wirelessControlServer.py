84d5183b29f353b73c9a22c11af7ce45fea339d4
simon0302010 <100756138+simon0302010@users.noreply.github.com>
2024-04-29 08:19:07 +0200

    added socket server

--- server/wirelessControlServer.py
+++ server/wirelessControlServer.py
@@ -1,3 +1,5 @@
+import socket
+
 def pass_to_correct_function(input_string):
     parts = input_string.split(':')
     if parts[0].lstrip('0') == '1':
@@ -49,4 +51,21 @@ def stop_video_stream():
     print("Video stream stopped")
 
 pass_to_correct_function("6:1")
+
+def open_socket_server():
+    host = '127.0.0.1'
+    port = 5000
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    s.listen(1)
+    conn, addr = s.accept()
+    print('Connected by', addr)
+    while True:
+        data = conn.recv(1024).decode()
+        if not data: break
+        #conn.sendall(data.encode())
+        pass_to_correct_function(data)
+    conn.close()
+
+
 
