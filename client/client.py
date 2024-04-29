94f780545753029a00db7be9db0af9938b6a4817
simon0302010 <simon0302010@gmail.com>
2024-04-29 15:25:17 +0200

    added python client

---
+++ client/client.py
@@ -0,0 +1,19 @@
+import socket
+
+host = 'localhost'
+port = 5000
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+if s.recv(1024).decode() == "init":
+    print("connection established")
+
+    while True:
+        try:
+            s.sendall(input("Send command: ").encode())
+            data = s.recv(1024).decode()
+            print(f"Response: {data}")
+        except Exception as e:
+            print(e)
+s.close()
